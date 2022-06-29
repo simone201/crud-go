855f65111dbb573bd6e51709e031a700c0b642d3
Simone Renzo <simone201@gmail.com>
2022-06-29 15:07:23 +0200

    Move createInContext into db utils
    Signed-off-by: Simone Renzo <s.renzo@perceptolab.com>

--- db/common.go
+++ db/common.go
@@ -1,10 +1,8 @@
 package db
 
 import (
-	"context"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
-	"log"
 )
 
 type Database interface {
@@ -14,20 +12,3 @@ type Database interface {
 }
 
 var Instance Database
-
-func createInContext(db *sql.DB, ctx context.Context, query string) (sql.Result, error) {
-	res, err := db.ExecContext(ctx, query)
-	if err != nil {
-		log.Printf("Error %s when creating table", err)
-		return nil, err
-	}
-
-	rows, err := res.RowsAffected()
-	if err != nil {
-		log.Printf("Error %s when getting rows affected", err)
-		return nil, err
-	}
-	log.Printf("Rows affected when creating table: %d", rows)
-
-	return res, nil
-}
--- db/utils.go
+++ db/utils.go
@@ -9,6 +9,23 @@ import (
 
 type RollbackFunc func(tx *sql.Tx)
 
+func createInContext(db *sql.DB, ctx context.Context, query string) (sql.Result, error) {
+	res, err := db.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when creating table", err)
+		return nil, err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when getting rows affected", err)
+		return nil, err
+	}
+	log.Printf("Rows affected when creating table: %d", rows)
+
+	return res, nil
+}
+
 func initBackgroundTransaction(db Database, ctx context.Context, timeout time.Duration) (*sql.Tx, context.Context, context.CancelFunc, RollbackFunc, error) {
 	ctx, cancelFunc := context.WithTimeout(ctx, timeout*time.Second)
 
